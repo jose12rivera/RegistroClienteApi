b4e9698d887976d64cbc9f0f44bfa44a2c076b05
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-02 19:23:03 -0400

    El contexto

--- Tecnicos.Data/DI/DbContextRegistar.cs
+++ Tecnicos.Data/DI/DbContextRegistar.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tecnicos.Data.Context;
 
 namespace Tecnicos.Data.DI;
 
-public static class DbContextRegistrar
+public static class DbContextRegistar
 {
     public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
     {
@@ -17,3 +12,4 @@ public static class DbContextRegistrar
         return services;
     }
 }
+
