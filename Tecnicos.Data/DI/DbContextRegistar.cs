1a9028d84d15d0126330d2bd612444edeaceaa7f
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-02 22:21:16 -0400

    El ContextRegistrar

--- Tecnicos.Data/DI/DbContextRegistar.cs
+++ Tecnicos.Data/DI/DbContextRegistar.cs
@@ -2,14 +2,18 @@
 using Microsoft.Extensions.DependencyInjection;
 using Tecnicos.Data.Context;
 
-namespace Tecnicos.Data.DI;
-
-public static class DbContextRegistar
+namespace Tecnicos.Data.DI
 {
-    public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
+    public static class DbContextRegistar
     {
-        services.AddDbContextFactory<ClientesContext>(o => o.UseSqlServer("Name=SqlConStr"));
-        return services;
+        public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
+        {
+            services.AddDbContextFactory<ClientesContext>(options =>
+                options.UseSqlServer("Name=SqlConStr",
+                    sqlOptions => sqlOptions.MigrationsAssembly("RegistroClienteApi")
+                )
+            );
+            return services;
+        }
     }
 }
-
